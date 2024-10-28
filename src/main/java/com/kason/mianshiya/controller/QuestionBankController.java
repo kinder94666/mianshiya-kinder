7174bc8522558c8ba4edafa26c47c7cab6d93b25
kinderMacbookPro <778651854@qq.com>
2024-10-28 15:07:01 +0800

    feat:删除题库题目

--- src/main/java/com/kason/mianshiya/controller/QuestionBankController.java
+++ src/main/java/com/kason/mianshiya/controller/QuestionBankController.java
@@ -1,20 +1,26 @@
 package com.kason.mianshiya.controller;
+
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.kason.mianshiya.annotation.AuthCheck;
+import com.kason.mianshiya.common.BaseResponse;
 import com.kason.mianshiya.common.ErrorCode;
 import com.kason.mianshiya.common.ResultUtils;
+import com.kason.mianshiya.constant.UserConstant;
 import com.kason.mianshiya.exception.ThrowUtils;
 import com.kason.mianshiya.model.dto.question.QuestionQueryRequest;
 import com.kason.mianshiya.model.dto.questionbank.QuestionBankQueryRequest;
+import com.kason.mianshiya.model.dto.questionbank.QuestionBankQuestionRemoveRequest;
 import com.kason.mianshiya.model.entity.Question;
 import com.kason.mianshiya.model.entity.QuestionBank;
+import com.kason.mianshiya.model.entity.QuestionBankQuestion;
 import com.kason.mianshiya.model.vo.QuetionBankVO;
-import com.kason.mianshiya.common.BaseResponse;
+import com.kason.mianshiya.service.IQuestionBankQuestionService;
 import com.kason.mianshiya.service.IQuestionBankService;
 import com.kason.mianshiya.service.IQuestionService;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
@@ -27,6 +33,9 @@ public class QuestionBankController {
     private IQuestionBankService questionBankService;
     @Resource
     private IQuestionService questionService;
+
+    @Resource
+    private IQuestionBankQuestionService questionBankQuestionService;
     @GetMapping("/get/vo")
     public BaseResponse<QuetionBankVO> getQuestionBankVO(QuestionBankQueryRequest questionQueryRequest, HttpServletRequest request) {
         ThrowUtils.throwIf(questionQueryRequest == null, ErrorCode.PARAMS_ERROR);
@@ -45,4 +54,28 @@ public class QuestionBankController {
         return ResultUtils.success(vo);
 
     }
+
+    /**
+     * 删除题目和题库之间的联系
+     * @param questionBankQuestionRemoveRequest 题目和题库id
+     * @return
+     */
+    @PostMapping("/remove")
+    @AuthCheck(mustRole = UserConstant.ADMIN_ROLE)
+    public BaseResponse<Boolean> removeQuestionBankQuestion(
+            @RequestBody QuestionBankQuestionRemoveRequest questionBankQuestionRemoveRequest
+    ) {
+        // 参数校验
+        ThrowUtils.throwIf(questionBankQuestionRemoveRequest == null, ErrorCode.PARAMS_ERROR);
+        Long questionBankId = questionBankQuestionRemoveRequest.getQuestionBankId();
+        Long questionId = questionBankQuestionRemoveRequest.getQuestionId();
+        ThrowUtils.throwIf(questionBankId == null || questionId == null, ErrorCode.PARAMS_ERROR);
+        // 构造查询
+        LambdaQueryWrapper<QuestionBankQuestion> lambdaQueryWrapper = Wrappers.lambdaQuery(QuestionBankQuestion.class)
+                .eq(QuestionBankQuestion::getQuestionId, questionId)
+                .eq(QuestionBankQuestion::getQuestionBankId, questionBankId);
+        boolean result = questionBankQuestionService.remove(lambdaQueryWrapper);
+        return ResultUtils.success(result);
+    }
+
 }
